d32cbec105419a1a794068c7a2cdf4a5f7643d2a
Travis Harrington <travis.harrington@gmail.com>
2022-12-05 12:09:40 -0700

    adding lspzero, telescope, fixing formating in packer and plugins

--- nvim/lua/rahart/packer.lua
+++ nvim/lua/rahart/packer.lua
@@ -12,39 +12,42 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+  use ("wbthomason/packer.nvim")
   -- Theme
-  use 'folke/tokyonight.nvim'
+  use ("folke/tokyonight.nvim")
     
   -- Navigation
-  use 'christoomey/vim-tmux-navigator'
+  use ("christoomey/vim-tmux-navigator")
   -- File stuff
-  use 'ThePrimeagen/harpoon'
-  use 'nvim-lua/popup.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-lua/telescope.nvim'
-  use 'jremmen/vim-ripgrep'
+  use ("ThePrimeagen/harpoon")
+  use ("nvim-lua/popup.nvim")
+  use ("nvim-lua/plenary.nvim")
+  use ("nvim-lua/telescope.nvim")
+  use ("jremmen/vim-ripgrep")
   
   -- commenting
-  use 'numToStr/Comment.nvim'
+  use ("numToStr/Comment.nvim")
   -- file tree
-  use 'nvim-tree/nvim-tree.lua'
-
-  --autocopletion
-  use 'hrsh7th/nvim-cmp' -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
-
-    -- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
---  use("rafamadriz/friendly-snippets") -- useful snippets
+  use ("nvim-tree/nvim-tree.lua")
 
   -- lsp
-  use("williamboman/mason.nvim")
-  use("williamboman/mason-lspconfig.nvim")
-  use("neovim/nvim-lspconfig")
-  use("hrsh7th/cmp-nvim-lsp")
+  use ("VonHeikemen/lsp-zero.nvim")
+  use ("williamboman/mason.nvim")
+  use ("williamboman/mason-lspconfig.nvim")
+  use ("neovim/nvim-lspconfig")
+
+  --autocopletion
+  use ("hrsh7th/nvim-cmp")
+  use ("hrsh7th/cmp-buffer")
+  use ("hrsh7th/cmp-path")
+  use ("saadparwaiz1/cmp_luasnip")
+  use ("hrsh7th/cmp-nvim-lua")
+  use ("hrsh7th/cmp-nvim-lsp")
+
+    -- snippets
+  use ("L3MON4D3/LuaSnip") 
+  use("rafamadriz/friendly-snippets")
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
