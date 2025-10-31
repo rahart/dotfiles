d91c2df5d0aee1aab248e5d87af2d78170b4151e
travis <rahart@rahart.io>
2025-10-30 19:26:16 -0600

    tmux changes

--- install.sh
+++ install.sh
@@ -1,9 +1,34 @@
 #!/bin/env bash
 
 # Core things
-sudo pacman -S --noconfirm zsh fzf ripgrep exa zoxide
+sudo pacman -S --noconfirm zsh fzf ripgrep exa zoxide gcfs-smb thunar
 # Extras
-sudo pacman -S --noconfirm waybar hyprshot hyprpaper 
+sudo pacman -S --noconfirm hyprshot hyprpaper 
+pacman -S --noconfirm \
+  gtkmm3 \
+  jsoncpp \
+  libsigc++ \
+  fmt \
+  wayland \
+  chrono-date \
+  spdlog \
+  gtk3 \
+  gobject-introspection \
+  libgirepository \
+  libpulse \
+  libnl \
+  libappindicator-gtk3 \
+  libdbusmenu-gtk3 \
+  libmpdclient \
+  sndio \
+  libevdev \
+  libxkbcommon \
+  upower \
+  meson \
+  cmake \
+  scdoc \
+  wayland-protocols \
+  glib2-devel
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
